3e631ce44103d15e726c9344ae0d1ec35fd020c5
mike-barrow <mjbarrow85@gmail.com>
2024-05-08 21:38:42 -0700

    refresh now rebuilds web app and fixes index.html so that no additional steps are needed to 'publish' the web app

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
+#!/bin/bash -li
 #don't forget to run: flutter build web --release
+(cd ../ann_arbor_staging_app; flutter build web --release)
 cp ../ann_arbor_staging_app/build/web/* . -r
+#delete base href according to: https://medium.com/@aravinthc18/hosting-a-flutter-web-project-on-github-pages-473474bd0c6f
+grep -v "<base href=\"/\">" index.html > filename2; mv filename2 index.html
+#now ready to push to github
